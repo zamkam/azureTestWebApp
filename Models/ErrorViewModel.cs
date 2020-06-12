ebdd5ede39c595df3058055b60c39b7fd49660bb
Sam Camargo <sam.camargo@relativity.com>
2020-06-12 14:45:35 -0500

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
